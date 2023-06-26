d4d640d890d6d5eec7af4a9205a2c12a68ab9428
Nohinkyu <wjd2256@naver.com>
2023-06-26 20:22:57 +0900

    feat: Item 클릭시 해당 url로 이동하도록 구현

--- app/src/main/java/com/nik/tkforum/ui/video/FragmentVideo.kt
+++ app/src/main/java/com/nik/tkforum/ui/video/FragmentVideo.kt
@@ -1,16 +1,30 @@
 package com.nik.tkforum.ui.video
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.nik.tkforum.BuildConfig
 import com.nik.tkforum.databinding.FragmentVideoBinding
+import com.nik.tkforum.ui.Constants
+import com.nik.tkforum.ui.TekkenForumApplication
+import com.nik.tkforum.ui.VideoClickListener
+import kotlinx.coroutines.launch
 
 class FragmentVideo : Fragment() {
 
     private var _binding: FragmentVideoBinding? = null
     private val binding get() = _binding!!
+    private val videoClickListener = object : VideoClickListener {
+        override fun onClick(url: String) {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,8 +35,27 @@ class FragmentVideo : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setLayout()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setLayout() {
+        val adapter = VideoAdapter(videoClickListener)
+        binding.rvVideoItemList.adapter = adapter
+        val appContainer = (activity?.application as TekkenForumApplication).appContainer
+        val apiClient = appContainer.provideApiClient()
+        lifecycleScope.launch {
+            val response = apiClient.getVideo(
+                BuildConfig.KAKAO_API_KEY,
+                Constants.RECOMMENDED_VIDEO_TAG.random()
+            )
+            adapter.submitList(response.documents)
+        }
     }
 }
