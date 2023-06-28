31f68392f4a68667825879a75a3a55528a0c0856
Nohinkyu <wjd2256@naver.com>
2023-06-29 07:59:11 +0900

    feat: 패키지 수정, TekkenForumApplication 코드 수정

--- app/src/main/java/com/nik/tkforum/ui/TekkenForumApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.nik.tkforum.ui
-
-import android.app.Application
-import com.nik.tkforum.network.AppContainer
-
-class TekkenForumApplication : Application() {
-
-    val appContainer = AppContainer()
-}
