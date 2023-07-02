7b28728337787cb2030eba5778e389c1e37d6266
Nohinkyu <wjd2256@naver.com>
2023-07-02 23:22:34 +0900

    feat: SettingFragment 에서 임시로 로그아웃 기능 구현

--- app/src/main/java/com/nik/tkforum/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/nik/tkforum/ui/setting/SettingFragment.kt
@@ -1,5 +1,8 @@
 package com.nik.tkforum.ui.setting
 
+import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.nik.tkforum.R
 import com.nik.tkforum.databinding.FragmentSettingBinding
 import com.nik.tkforum.ui.BaseFragment
@@ -8,4 +11,17 @@ class SettingFragment : BaseFragment() {
 
     override val binding get() = _binding as FragmentSettingBinding
     override val layoutId: Int get() = R.layout.fragment_setting
+    private val firebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLogout.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        firebaseAuth.signOut()
+    }
 }
