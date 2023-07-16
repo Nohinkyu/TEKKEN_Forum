a02608fa20ccf34ae3138ab30b5fc29e0ee6da7a
Nohinkyu <wjd2256@naver.com>
2023-07-16 23:33:18 +0900

    feat: 시즌정보, 캐릭터 보여주는 RecyclerView 멀티플뷰타입으로 구현

---
+++ app/src/main/java/com/nik/tkforum/ui/home/CharacterListAdapter.kt
@@ -0,0 +1,94 @@
+package com.nik.tkforum.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import coil.load
+import com.nik.tkforum.databinding.ItemCharacterBinding
+import com.nik.tkforum.databinding.ItemHeaderCharacterListBinding
+
+private const val ITEM_TYPE_HEADER = 0
+private const val ITEM_TYPE_CHARACTER = 1
+
+class CharacterListAdapter() : RecyclerView.Adapter<ViewHolder>() {
+
+    private val characterList = mutableListOf<CharacterListSection>()
+
+    override fun getItemViewType(position: Int): Int {
+        return when (characterList[position]) {
+            is CharacterListSection.CharacterListHeader -> ITEM_TYPE_HEADER
+            is CharacterListSection.Character -> ITEM_TYPE_CHARACTER
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return when (viewType) {
+            ITEM_TYPE_HEADER -> HeaderViewHolder.from(parent)
+            ITEM_TYPE_CHARACTER -> CharacterViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown ViewType $viewType")
+        }
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (holder) {
+            is HeaderViewHolder -> {
+                val item = characterList[position] as CharacterListSection.CharacterListHeader
+                holder.bind(item)
+            }
+
+            is CharacterViewHolder -> {
+                val item = characterList[position] as CharacterListSection.Character
+                holder.bind(item)
+            }
+        }
+    }
+
+
+    override fun getItemCount(): Int {
+        return characterList.size
+    }
+
+    fun submitList(list: MutableList<CharacterListSection>) {
+        characterList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    class HeaderViewHolder(val binding: ItemHeaderCharacterListBinding) : ViewHolder(binding.root) {
+
+        fun bind(characterData: CharacterListSection.CharacterListHeader) {
+            binding.tvHeaderCharacterList.text = characterData.season
+        }
+
+        companion object {
+            fun from(
+                parent: ViewGroup,
+            ): HeaderViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                return HeaderViewHolder(
+                    ItemHeaderCharacterListBinding.inflate(inflater, parent, false)
+                )
+            }
+        }
+    }
+
+    class CharacterViewHolder(val binding: ItemCharacterBinding) : ViewHolder(binding.root) {
+
+        fun bind(characterData: CharacterListSection.Character) {
+            binding.tvCharacterName.text = characterData.character.characterName
+            binding.ivCharacterImage.load(characterData.character.characterImage)
+        }
+
+        companion object {
+            fun from(
+                parent: ViewGroup,
+            ): CharacterViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                return CharacterViewHolder(
+                    ItemCharacterBinding.inflate(inflater, parent, false)
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/nik/tkforum/ui/home/CharacterListSection.kt
@@ -0,0 +1,23 @@
+package com.nik.tkforum.ui.home
+
+import com.nik.tkforum.data.CharacterData
+
+const val CHARACTER_LIST_SEASON = 0
+const val CHARACTER_LIST_CHARACTER = 1
+
+sealed class CharacterListSection {
+
+    data class CharacterListHeader(val season: String) : CharacterListSection() {
+
+        override val id: Int
+            get() = CHARACTER_LIST_SEASON
+    }
+
+    data class Character(val character: CharacterData) : CharacterListSection() {
+
+        override val id: Int
+            get() = CHARACTER_LIST_CHARACTER
+    }
+
+    abstract val id: Int
+}
