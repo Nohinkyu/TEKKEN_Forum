362bfa525b22b4eb47cb3f6dc6c7a99171bf3b78
Nohinkyu <wjd2256@naver.com>
2023-07-22 22:27:13 +0900

    feat: PreferenceManager 앱 전역에서 접근할 수 있게 구현

--- app/src/main/java/com/nik/tkforum/TekkenForumApplication.kt
+++ app/src/main/java/com/nik/tkforum/TekkenForumApplication.kt
@@ -1,6 +1,7 @@
 package com.nik.tkforum
 
 import android.app.Application
+import com.nik.tkforum.data.PreferenceManager
 import com.nik.tkforum.network.AppContainer
 
 class TekkenForumApplication : Application() {
@@ -8,9 +9,11 @@ class TekkenForumApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appContainer = AppContainer()
+        preferencesManager = PreferenceManager(this)
     }
 
     companion object {
         lateinit var appContainer: AppContainer
+        lateinit var preferencesManager: PreferenceManager
     }
 }
