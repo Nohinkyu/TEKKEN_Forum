672e43b076fa13155d74777d7fd42f8b1149844b
Nohinkyu <wjd2256@naver.com>
2023-07-29 23:21:52 +0900

    feat: ChatRoomFragment MVVM 적용

--- app/src/main/java/com/nik/tkforum/ui/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/nik/tkforum/ui/chatroom/ChatRoomFragment.kt
@@ -1,37 +1,32 @@
 package com.nik.tkforum.ui.chatroom
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.nik.tkforum.BuildConfig
 import com.nik.tkforum.R
 import com.nik.tkforum.TekkenForumApplication
 import com.nik.tkforum.data.Chat
 import com.nik.tkforum.data.User
 import com.nik.tkforum.databinding.FragmentChatRoomBinding
+import com.nik.tkforum.repository.ChatRoomRepository
 import com.nik.tkforum.ui.BaseFragment
 import com.nik.tkforum.util.Constants
-import kotlinx.coroutines.launch
 
 class ChatRoomFragment : BaseFragment() {
 
     override val binding get() = _binding as FragmentChatRoomBinding
     override val layoutId: Int get() = R.layout.fragment_chat_room
 
-    private val appContainer = TekkenForumApplication.appContainer
-    private val apiClient = appContainer.provideGoogleApiClient()
     private val args: ChatRoomFragmentArgs by navArgs()
 
-    private val adapter = ChatListAdapter()
-
-    private var result = mutableListOf<ChatType>()
+    private val viewModel by viewModels<ChatRoomViewModel> {
+        ChatRoomViewModel.provideFactory(
+            repository = ChatRoomRepository(
+                TekkenForumApplication.appContainer.provideGoogleApiClient()
+            )
+        )
+    }
 
     private val user = User(
         TekkenForumApplication.preferencesManager.getString(Constants.KEY_PROFILE_IMAGE, ""),
@@ -42,7 +37,7 @@ class ChatRoomFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setLayout()
-        updateChat()
+        viewModel.addChatListener(args.chatRoomKey)
 
         val editText = binding.etChat
         binding.btSend.setOnClickListener {
@@ -54,36 +49,12 @@ class ChatRoomFragment : BaseFragment() {
     }
 
     private fun setLayout() {
+        val adapter = ChatListAdapter()
         binding.rvChatList.adapter = adapter
-        lifecycleScope.launch {
-            val response = apiClient.getChatList(args.chatRoomKey)
-            try {
-                if (response.isSuccessful) {
-                    val data = response.body()
-                    data?.let {
-                        setAdapterChatList(data.values.toList())
-                        adapter.submitList(result)
-                        if (result.size > 0) {
-                            binding.rvChatList.scrollToPosition(result.size - 1)
-                        }
-                    }
-                } else {
-                    val errorMessage = response.errorBody().toString()
-                    Log.d("ChatFragment", errorMessage)
-                }
-            } catch (e: Exception) {
-                Log.d("ChatFragment", "데이터를 가져오지 못했습니다.")
-            }
-        }
-    }
-
-    private fun setAdapterChatList(chatList: List<Chat>) {
-        val email = FirebaseAuth.getInstance().currentUser?.email
-        for (chat in chatList) {
-            if (chat.senderEmail == email) {
-                result.add(ChatType.SentChat(chat))
-            } else
-                result.add(ChatType.ReceivedChat(chat))
+        viewModel.loadChatList(args.chatRoomKey)
+        viewModel.chatList.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+            binding.rvChatList.scrollToPosition(adapter.currentList.size - 1)
         }
     }
 
@@ -94,37 +65,6 @@ class ChatRoomFragment : BaseFragment() {
             binding.etChat.text.toString(),
             user.email
         )
-        lifecycleScope.launch {
-            apiClient.sendChat(args.chatRoomKey, chat)
-        }
+        viewModel.sendChat(args.chatRoomKey, chat)
     }
-
-    private fun updateChat() {
-        binding.rvChatList.adapter = adapter
-        FirebaseDatabase.getInstance(BuildConfig.GOOGLE_BASE_URL).getReference("chatRoomList")
-            .child(args.chatRoomKey).child("chatList")
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    result.clear()
-                    for (data in snapshot.children) {
-                        val data = data.getValue(Chat::class.java)
-                        if (data?.senderEmail == user.email) {
-                            result.add(ChatType.SentChat(data))
-                        } else {
-                            result.add(ChatType.ReceivedChat(data!!))
-                        }
-                        adapter.notifyDataSetChanged()
-                        if (result.size > 0) {
-                            binding.rvChatList.scrollToPosition(result.size - 1)
-                        }
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Log.d("ChatFragment", "데이터를 가져오지 못했습니다.")
-                }
-            })
-    }
-
-
 }
---
+++ app/src/main/java/com/nik/tkforum/ui/chatroom/ChatRoomViewModel.kt
@@ -0,0 +1,96 @@
+package com.nik.tkforum.ui.chatroom
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.nik.tkforum.BuildConfig
+import com.nik.tkforum.TekkenForumApplication
+import com.nik.tkforum.data.Chat
+import com.nik.tkforum.repository.ChatRoomRepository
+import com.nik.tkforum.util.Constants
+import kotlinx.coroutines.launch
+
+class ChatRoomViewModel(private val repository: ChatRoomRepository) : ViewModel() {
+
+    private val senderEmail = TekkenForumApplication.preferencesManager.getString(
+        Constants.KEY_MAIL_ADDRESS, ""
+    )
+
+    private var chatListener: ChildEventListener? = null
+
+    private val dataBase = FirebaseDatabase.getInstance(BuildConfig.GOOGLE_BASE_URL)
+
+    private val _chatList = MutableLiveData<List<ChatType>>()
+    val chatList: LiveData<List<ChatType>> = _chatList
+
+    fun loadChatList(chatRoomKey: String) {
+        viewModelScope.launch {
+            val response = repository.getChatList(chatRoomKey)
+            val chatTypeList = mutableListOf<ChatType>()
+            val data = response.body()
+            data?.let {
+                for (chat in data.values) {
+                    if (chat.senderEmail == senderEmail) {
+                        chatTypeList.add(ChatType.SentChat(chat))
+                    } else {
+                        chatTypeList.add(ChatType.ReceivedChat(chat))
+                    }
+                }
+            }
+            _chatList.value = chatTypeList
+        }
+    }
+
+    fun sendChat(chatRoomKey: String, chat: Chat) {
+        viewModelScope.launch {
+            repository.sendChat(chatRoomKey, chat)
+        }
+    }
+
+    fun addChatListener(chatRoomKey: String) {
+        val chatTypeList = mutableListOf<ChatType>()
+        chatListener = dataBase.getReference("chatRoomList")
+            .child(chatRoomKey).child("chatList")
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(dataSnapshot: DataSnapshot, prevChildKey: String?) {
+                    val newMessage = dataSnapshot.getValue(Chat::class.java) ?: return
+                    if (newMessage.senderEmail == senderEmail) {
+                        chatTypeList.add(ChatType.SentChat(newMessage))
+                    } else {
+                        chatTypeList.add(ChatType.ReceivedChat(newMessage))
+                    }
+                    _chatList.value = chatTypeList
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        chatListener = null
+    }
+
+    companion object {
+
+        fun provideFactory(repository: ChatRoomRepository) = viewModelFactory {
+            initializer {
+                ChatRoomViewModel(repository)
+            }
+        }
+    }
+}
